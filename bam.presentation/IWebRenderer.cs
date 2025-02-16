641c053d80b8d23d2514e0de75067df92b918817
Bryan Apellanes <bryan.apellanes@gmail.com>
2025-02-16 09:13:50 -0600

    cleanup usings

--- bam.presentation/IWebRenderer.cs
+++ bam.presentation/IWebRenderer.cs
@@ -1,12 +1,6 @@
 /*
     Copyright © Bryan Apellanes 2015
 */
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
 
 namespace Bam.Presentation
 {
